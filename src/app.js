13c4b2d6f575b71b8a2bcf8b7c963abbd3449d0b
Daniel Guerrero <porta11271127@gmail.com>
2022-06-10 23:49:23 -0600

    app file  updated with new paths created

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express  from "express";
-
+import {travelRouter}  from "./routes/travel.routes.js";
+import {driverRouter} from "./routes/driver.routes.js";
+import  {userRouter}  from "./routes/user.routes.js";
+import { busRouter } from "./routes/bus.controller.js";
 
 const app = express();
 
-app.get('/', (req, res) => { 
+app.use(express.json());
 
-    res.send({
-        'API': 'working! :)'
-    })
- })
-
+app.use(driverRouter)
+app.use(travelRouter)
+app.use(userRouter)
+app.use(busRouter)
  export default app
